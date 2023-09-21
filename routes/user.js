31c2c382a0a256285427f4cbbf69dd11ba0fc91d
audwo <kmjstj3@naver.com>
2023-09-21 19:23:47 +0900

    Add findUserByI d Middleware

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { isLoggedIn } = require("../middlewares");
 const { follow } = require("../controllers/user");
+const { findUserById } = require("../controllers/user");
 
+router.get("/:id", isLoggedIn, findUserById);
 router.post("/:id/follow", isLoggedIn, follow);
 
 module.exports = router;
