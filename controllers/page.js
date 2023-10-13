c1aed4b8a1a54fb10583c0e6a0c61ab5d1cd6d22
audwo <kmjstj3@naver.com>
2023-10-13 16:00:18 +0900

    Add renderStoryReplyComments controller

--- controllers/page.js
+++ controllers/page.js
@@ -1,6 +1,11 @@
 const User = require("../models/user");
 const Post = require("../models/post");
-//서비스를 호출하는 코
+const Story = require("../models/story");
+//서비스를 호출하는 코\\
+
+const PostComment = require("../models/postComment");
+const StoryComment = require("../models/storyComment");
+const DiaryComment = require("../models/diaryComment");
 
 exports.renderMain = async (req, res, next) => {
   if (res.locals.user) {
@@ -41,3 +46,94 @@ exports.renderPosts = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.renderPostsComments = async (req, res, next) => {
+  try {
+    const postsComments = await PostComment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+        {
+          model: Post,
+          attributes: ["id"],
+        },
+      ],
+    });
+
+    const onlyInfoPostsComments = postsComments.map(
+      (comments) => comments.dataValues
+    );
+    res.send(onlyInfoPostsComments);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.renderStory = async (req, res, next) => {
+  try {
+    const stories = await Story.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nickname"],
+      },
+    });
+
+    const onlyInfoStory = stories.map((story) => story.dataValues);
+    res.send(onlyInfoStory);
+    console.log("10101010101010");
+    console.log(onlyInfoStory);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.renderStoryComments = async (req, res, next) => {
+  try {
+    const storyComments = await StoryComment.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nickname"],
+      },
+    });
+
+    const onlyInfoStoryComments = storyComments.map(
+      (comments) => comments.dataValues
+    );
+    res.send(onlyInfoStoryComments);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.renderStroyReplyComments = async (req, res, next) => {
+  const storyCommentId = req.body.StoryCommentId;
+  try {
+    const replyComments = await StoryComment.findAll({
+      where: { StoryId: storyCommentId },
+      include: {
+        model: User,
+        attributes: ["id", "nickname"],
+      },
+    });
+
+    const onlyInfoStoryReplyComments = replyComments.map(
+      (comment) => comment.dataValues
+    );
+    res.send(onlyInfoStoryReplyComments);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/*
+
+exports.renderStoryComments = async(req,res,next)=> {
+  try{
+    const storyComments = await StoryComment.findAll()
+
+  } catch(error){
+
+  }
+}*/
