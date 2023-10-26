139ec56fc83f56bf3c3e03b748e0e3fc37fe9147
audwo <kmjstj3@naver.com>
2023-10-26 21:59:18 +0900

    사용자 업데이터 컨트롤러 추가

--- controllers/update.js
+++ controllers/update.js
@@ -1,24 +1,62 @@
 const Post = require("../models/post");
+const PostComment = require("../models/postComment");
 const Story = require("../models/story");
 
-exports.increaseLikeCount = async (req, res, next) => {
+exports.updatePostLikeCount = async (req, res, next) => {
   try {
     const postId = req.body.id;
     const likeCount = req.body.likeCount;
+    const likeCheck = req.body.likeCheck;
 
     const updatePost = await Post.update(
-      { likeCount: likeCount, likeCheck: true },
+      { likeCount: Number(likeCount), likeCheck: likeCheck },
       { where: { id: postId } }
     );
+    res.status(200).json({ message: "Story updated successfully" });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    if (postId[0] === 1) {
+exports.updateCommentLikeCount = async (req, res, next) => {
+  try {
+    const commentId = req.body.id;
+    const likeCount = req.body.likeCount;
+    const likeCheck = req.body.likeCheck;
+
+    const updatePost = await PostComment.update(
+      { likeCount: Number(likeCount), likeCheck: likeCheck },
+      { where: { id: commentId } }
+    );
+    res.status(200).json({ message: "Story updated successfully" });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.updateUserInfo = async (req, res, next) => {
+  try {
+    const updateUser = await User.update(
+      {
+        nickname: req.body.nickname,
+        profileImg: req.body.img,
+        name: req.body.name,
+      },
+      { where: { id: req.user.id } }
+    );
+    res.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * 
+ *  if (updatePost[0] === 1) {
       // 업데이트 성공
       res.status(200).json({ message: "Story updated successfully" });
     } else {
       // 업데이트할 스토리를 찾지 못한 경우
       res.status(404).json({ message: "Story not found" });
     }
-  } catch (error) {
-    console.error(error);
-  }
-};
+ */
