27cdbbdfcfdf43a1059b75079b97c18647df10a2
audwo <kmjstj3@naver.com>
2023-11-20 00:08:07 +0900

    Post Model

--- models/post.js
+++ models/post.js
@@ -5,12 +5,46 @@ class Post extends Sequelize.Model {
     Post.init(
       {
         content: {
-          type: Sequelize.STRING(140),
-          allowNull: false,
+          type: Sequelize.STRING(500),
+          allowNull: true,
         },
         img: {
           type: Sequelize.STRING(140),
           allowNull: true,
+        },
+        title: {
+          type: Sequelize.STRING(140),
+          allowNull: true,
+        },
+        likeCheck: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        },
+        likeControl: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        },
+        commentControl: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        },
+        contentControl: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        },
+        likeCount: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          defaultValue: 0,
+        },
+        commentCount: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          defaultValue: 0,
         },
       },
       {
@@ -28,6 +62,9 @@ class Post extends Sequelize.Model {
   static associate(db) {
     db.Post.belongsTo(db.User);
     db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" });
+    db.Post.belongsToMany(db.User, {
+      through: "PostLike",
+    });
   }
 }
 
